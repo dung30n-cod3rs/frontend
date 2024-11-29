21fc75e96ca4dd92b78a58918e9782e83694c170
4aykovski <kan89kan@gmail.com>
2024-11-29 20:38:00 +0300

    fixes

--- src/app/(protected)/company/manage/page.tsx
+++ src/app/(protected)/company/manage/page.tsx
@@ -1,14 +1,3 @@
 export default function Page() {
-  const company: Company = {
-    id: 1,
-    name: "test name",
-  };
-
-  return (
-    <div className="flex flex-col gap-4 w-full">
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        {company.name}
-      </h2>
-    </div>
-  );
+  return <></>;
 }
